ac4e8b439991bcc70b82bf7daa8105b0d4d63e6b
Ryan Parsa <imryanparsa@gmail.com>
2025-01-26 05:00:18 -0800

    set ip

--- main.go
+++ main.go
@@ -11,6 +11,6 @@ func handler(w http.ResponseWriter, r *http.Request) {
 
 func main() {
 	http.HandleFunc("/", handler)
-	fmt.Println("Starting server on :8080")
-	http.ListenAndServe(":8080", nil)
+	fmt.Println("Starting server on 0.0.0.0:8080")
+	http.ListenAndServe("0.0.0.0:8080", nil)
 }
